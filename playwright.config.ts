9cf9bae4c37e000864bf8cbf36a996075019ba33
pooja A J <poojajayaraj1032@gmail.com>
2026-07-08 21:50:53 +0530

    Add Allure support

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,8 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
  reporter: [
-    ['html', { open: 'always' }]   // opens browser automatically after run
+    ['html', { open: 'always' }],  // opens browser automatically after run
+    ['allure-playwright']
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
